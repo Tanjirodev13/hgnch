09217ff024108af2b708bdc45112348694be6141
Tanjirodev13 <168975150+Tanjirodev13@users.noreply.github.com>
2024-06-28 19:50:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const { prefix, token } = require('./config.json');
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.once('ready', () => {
+  console.log(`Bot ${client.user.tag} is logged in!`);
+    client.user.setPresence({ activities: [{ name: 'Milestone', type: 'WATCHING' }], status: 'dnd' });// Set the bot's watching status
+});
+
+client.on('messageCreate', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'say') {
+    const text = args.join(' ');
+    message.channel.send(text);
+  }
+});
+
+client.login(process.env.token);
