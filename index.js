3c4d4f260cf561cac32da8403db6d985eca9bacf
Tanjirodev13 <168975150+Tanjirodev13@users.noreply.github.com>
2024-07-07 21:37:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {
     Client,
     Intents
 } = require('discord.js');
-const keepAliveServer = require('./keep_alive.js');
+
 
 const bot = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
